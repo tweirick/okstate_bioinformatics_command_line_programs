9d24f4e3a98a202cd5751d0d0ee880d46233760b
tweirick <tyler.weirick@gmail.com>
2014-01-24 17:45:03 -0600

    Update repair_fasta.py
    Added checks for O. Found some in viral protein sequences.

--- fasta_manipulation_programs/repair_fasta.py
+++ fasta_manipulation_programs/repair_fasta.py
@@ -9,92 +9,72 @@ def getargs(ver='%prog 0.0'):
     parser = argparse.ArgumentParser(description='Take a blastclust cluster '+
              'file as input and remove all clusters from fasta file set.')    
     parser.add_argument('--file_set', 
-                        #action='store_const',
-                        #const=sum, 
-                        #default=max,
                         help='')
     
     parser.add_argument('--file_suffix', 
-                        #action='store_const',
-                        #const=sum, 
-                        default=".pruneBJXZ.fasta",
+                        default=".pruneBJXZU.fasta",
                         help='')
     
-
     parser.add_argument('--remove_over_x_percent', 
-                        #action='store_const',
-                        #const=sum, 
-                        #default=max,
+                        default=0.0,
                         help='')
+
     args = parser.parse_args()
     
     return glob(args.file_set),args.file_suffix,args.remove_over_x_percent
 
 
 def build_fasta_dict(input_file_name):
-    
-    #Make a dictionary from the fasta file used for input.
-    fasta_dict     = {}
-    temp_fasta_seq = []
-    fasta_name     = ""
-    
-    input_fasta_count = 0
-    
-    for line in open(input_file_name,'r'):
-        
-        if ">" in line:
-            input_fasta_count+=1
-            if fasta_name != "":
-                fasta_dict.update(
-                    {fasta_name.strip():''.join(temp_fasta_seq)})
+    fasta_seq_name_list = []
+    temp_fasta_seq      = []
+    fasta_name          = None 
+    file = open(input_file_name,'r')
+
+    while True:
+        line = file.readline()
+        if line == "" or line[0] == ">":
+
+            if fasta_name != None:
+                fasta_seq_name_list.append([fasta_name,''.join(temp_fasta_seq)])
                 temp_fasta_seq = []
+            
+            if line == "": break
             fasta_name = line.strip()
         else:
-            temp_fasta_seq.append(line.strip())
-    #Catch last entry
-   
-    fasta_dict.update({fasta_name:''.join(temp_fasta_seq)})   
-
-    return fasta_dict,input_fasta_count
-
-def invert_dict(fasta_dict):
+            temp_fasta_seq.append(line.strip()) 
     
-    new_dict = {}
+    return fasta_seq_name_list
+
     
-    for e in fasta_dict:
-        new_dict.update({fasta_dict[e]:e})
+def buildnewfasta(fasta_name_seq_list,max_percent_illegal_chars):
+    """
+    This will discard function will splite the new_dict into two lists. One
+    with the entries to keep and the other to discard. This is based on 
+    whether there is a less than or equal to percentage of illegal chars in
+    the sequence than remove_over_x_percent
+    """
+    illegal_char_set = {"B","J","X","Z","U","O","0"}    
+
+    out_list, discard_list = [],[]
+    for fasta_name_seq in fasta_name_seq_list:
+        fasta_name,fasta_seq = fasta_name_seq[0],fasta_name_seq[1]
+        illegal_char_cnt = 0
         
-    return new_dict
-    
-    
-def buildnewfasta(new_dict,remove_over_x_percent):
-    
-    
-    out_list = []
-    for e in new_dict:
-        #print(remove_over_x_percent)
-        if remove_over_x_percent != None:
-            #print("prune X")
-            total_len = len(e)
-            x_cnt = 0
-            #This could probably be done with the count() function
-            #but I have encountered problems before with large sets of text 
-            #before when using some of the list functions. 
-            #print(e)
-            for aa in e:
-                if aa.upper() == "B" or aa.upper() == "J" or aa.upper() == "X" or aa.upper() == "Z":
-                   x_cnt+=1 
-                   
-            #print(float(x_cnt)/float(total_len),float(remove_over_x_percent))
-            if float(x_cnt)/float(total_len) < float(remove_over_x_percent):    
-                out_list.append(new_dict[e]+'\n'
-                        +e+'\n')
-            #else:
-            #    print("!!!!!!!!!!!!!!!!!!!!")
+        #This could be done with the count() function but I have encountered 
+        #problems with count() and very large strings. It was returning 
+        #incorrect results silently.  
+        for aa in fasta_seq:
+            if aa.upper() in illegal_char_set:
+                illegal_char_cnt+=1.0 
+        fasta_entry = "\n".join(fasta_name_seq)
+        percent_illegal_chars = illegal_char_cnt/float(len(fasta_seq))
+        assert type(percent_illegal_chars) == float
+        if percent_illegal_chars <= float(max_percent_illegal_chars):    
+            out_list.append(fasta_entry)
         else:
-            out_list.append(new_dict[e]+'\n'
-                        +e+'\n')
-    return out_list
+            discard_list.append(fasta_entry)
+
+    return out_list,discard_list
 
 
 
@@ -102,17 +82,22 @@ def buildnewfasta(new_dict,remove_over_x_percent):
 #file_set = glob("/home/TWeirick/FEATURE_GENERATING_PROGRAMS/40per_Sets/40per_all_catted.faa")#
 
 file_set,file_suffix,remove_over_x_percent = getargs(ver='%prog 0.0')
-
+print("File_Name","Fastas_In_Input","Fastas_In_Kept_Output","Files_In_Discarded_Output")
 for file_name in file_set:
     
-    fasta_dict,input_fasta_count = build_fasta_dict(file_name)
-    
-    new_dict = invert_dict(fasta_dict)
-    
-    out_list = buildnewfasta(new_dict,remove_over_x_percent)
-    
-    print(file_name,str(input_fasta_count),str(len(out_list)))
+    fasta_list = build_fasta_dict(file_name)
+   
+    out_list,discard_list = buildnewfasta(fasta_list,remove_over_x_percent)
+
+    print(file_name,
+          str(len(fasta_list)),
+          str(len(out_list)),
+          str(len(discard_list)))
+
     out_file = open(file_name+file_suffix,'w')
-    out_file.write(''.join(out_list))
+    out_file.write('\n'.join(out_list))
     out_file.close()
     
+    out_file = open(file_name+file_suffix+".discard",'w')
+    out_file.write('\n'.join(discard_list))
+    out_file.close()
